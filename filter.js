8bf5b28a3e2ccc77892ce10549ef149fe0de1d48
DickElliot <74756814+DickElliot@users.noreply.github.com>
2021-09-24 23:23:25 +1200

    Pseudocode for adding language filter

--- filter.js
+++ filter.js
@@ -1,1 +1,11 @@
-document.body.style.border = '5px solid red';
+// get all ids of current films
+// 'data-film-id' will get me the id
+// currently on the page. Though that means
+// that it'll only work on the page currently loaded.
+// A way to get the other films? Could be the Letterboxd API,
+// though that would require them giving me access.
+// jquery a lil selector for the lists, have language sent off then chosen.
+// Possibly load when the page is loaded, though that's going to have issues
+// with the loading of the pages, ergo a page of nearly all English would become
+// a page with 3 films on it. To get around this I'd have to take-over the entire
+// pagination. Possible?
