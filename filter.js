d709f82a2c64dd7bf0c5bde4a97ae080e6116e03
DickElliot <74756814+DickElliot@users.noreply.github.com>
2021-10-02 23:00:11 +1300

    Added tags to scrape

--- filter.js
+++ filter.js
@@ -1,14 +1,10 @@
-import { fetchTMDBData } from './tmdb';
-// get all ids of current films
-// 'data-film-id' will get me the id
-// currently on the page. Though that means
-// that it'll only work on the page currently loaded.
-// A way to get the other films? Could be the Letterboxd API,
-// though that would require them giving me access.
-// jquery a lil selector for the lists, have language sent off then chosen.
-// Possibly load when the page is loaded, though that's going to have issues
-// with the loading of the pages, ergo a page of nearly all English would become
-// a page with 3 films on it. To get around this I'd have to take-over the entire
-// pagination. Possible?
+// import { fetchTMDBData } from './tmdb';
+// const request = require('request');
+
+console.log(document.body);
 let movieIds = [];
 let menuElement;
+
+// Scrape tags
+// data-film-name
+// data-film-release-year name
--- tmdb.ts
+++ tmdb.ts
@@ -1,19 +1,21 @@
-export type Movie =  {
+export type Movie = {
   language: string;
   title: string;
   year: number;
-}
+};
 
 export const fetchTMDBData = async (id) => {
   const apiKey = '';
-  const endpoint =
-    `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&language=en-US&query=insert%20here&page=1&include_adult=false`;
+  const endpoint = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&language=en-US&query=insert%20here&page=1&include_adult=false`;
   const data = await (await fetch(endpoint)).json();
-  return data.results.map((movies) => ({
+  return data.results;
+  /* .map((movies) => ({
     return<Movie> {
       language: movies.original_language,
       title: movies.original_title,
       year: movies.release_year, 
-    }
-  }));
+    } */
 };
+// )
+// );
+// };
