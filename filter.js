e73363a82a1254aece754caf636eded3dc3b233e
DickElliot <74756814+DickElliot@users.noreply.github.com>
2021-10-05 21:51:31 +1300

    Merged fetch function onto main file

--- filter.js
+++ filter.js
@@ -1,25 +1,48 @@
-// import * as cheerio from 'cheerio';
-const cheerio = require('cheerio');
-// import { fetchTMDBData } from './tmdb';
+// const fetchTMDBData = require('./tmdb');
+const fetchTMDBData = async (query) => {
+  let safeQuery = encodeURIComponent(query);
+  const apiKey = '6b3966c85ef3ac126edf60c04d0f2b22';
+  const endpoint = `http://api.themoviedb.org/3/search/movie?api_key=${apiKey}&language=en-US&query=${safeQuery}&page=1&include_adult=false`;
+  const data = await (
+    await fetch(endpoint, {
+      headers: {
+        Accept: '*/*',
+        Referer: 'https://letterboxd.com',
+        Origin: 'https://letterboxd.com',
+      },
+    })
+  ).json();
+  return data.results.map((movies) => {
+    let thisDate = movies.release_date.split('-')[0];
+    console.log(`fetch response: ${movies}`);
+    return {
+      language: movies.original_language,
+      title: movies.original_title,
+      year: thisDate,
+    };
+  });
+};
 
-function scanApple() {
-  const $ = cheerio.load(`<ul id="fruits">
-  <li class="apple">Apple</li>
-  <li class="orange">Orange</li>
-  <li class="pear">Pear</li>
-</ul>`);
-  let result = $('.apple', '#fruits').text();
-  console.log(result);
-  return result;
-}
-module.exports = scanApple;
 console.log('***');
-// console.log($('li').attr('data-film-name'));
+let moviesStrings = [];
+const listElements = document.querySelectorAll('div[data-film-release-year]');
+listElements.forEach((listEl) => {
+  let year = listEl.getAttribute('data-film-release-year');
+  let title = listEl.getAttribute('data-film-name');
+  console.log(title, ' ', year);
+  let movieString = title + ' ' + year;
+  moviesStrings.push(movieString);
+});
+let movieResponseObjects = [];
+// /*
+moviesStrings.forEach((movie) => {
+  let movieObj = {};
+  console.log(`movieString: ${movie}`);
+  let result = fetchTMDBData(movie);
+  // console.log(`result: ${result}`);
+});
+// movieResponseObjects.forEach((movieObj) => {
+// console.log(`Title: ${movieObj.title} Year: ${movieObj.year} Language: ${movieObj.language}`);
+// });
+//  */
 console.log('###');
-// console.log(document.body);
-let movieIds = [];
-
-let menuElement;
-// Scrape tags
-// data-film-name
-// data-film-release-year name
--- tmdb.ts
+++ tmdb.ts
@@ -1,21 +1,49 @@
 export type Movie = {
-  language: string;
+  language?: string;
   title: string;
   year: number;
 };
 
-export const fetchTMDBData = async (id) => {
+export class Date {
+  year: number;
+  month: number;
+  day: number;
+  constructor(release_date) {
+    let dateParts = release_date.split('-');
+    this.year = dateParts[0];
+    this.month = dateParts[1];
+    this.day = dateParts[2];
+  }
+}
+
+export type TMDBResponse = {
+  poster_path: string;
+  adult: boolean;
+  overview: string;
+  release_date: string;
+  genre_ids: number[];
+  id: number;
+  original_title: string;
+  original_language: string;
+  title: string;
+  backdrop_path: string;
+  popularity: number;
+  vote_count: number;
+  video: boolean;
+  vote_average: number;
+};
+
+export const fetchTMDBData = async (query) => {
   const apiKey = '';
-  const endpoint = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&language=en-US&query=insert%20here&page=1&include_adult=false`;
+  const endpoint = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&language=en-US&query=${query}&page=1&include_adult=false`;
   const data = await (await fetch(endpoint)).json();
-  return data.results;
-  /* .map((movies) => ({
-    return<Movie> {
+  return <Movie>data.results.map((movies: TMDBResponse) => {
+    let thisDate = new Date(movies.release_date);
+    console.log(movies);
+    return <Movie>{
       language: movies.original_language,
       title: movies.original_title,
-      year: movies.release_year, 
-    } */
+      year: thisDate.year,
+    };
+  });
 };
-// )
-// );
-// };
